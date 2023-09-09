cd5e8545417f064222c07dbc0126f236dbc6bc4f
Lleyton Gray <lleyton@fyralabs.com>
2023-09-08 21:35:38 -0700

    more carousel stuff

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,6 +1,10 @@
 import { Tab } from "@headlessui/react";
 import { Fragment, type FC } from "react";
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 interface CarouselItem {
   name: string;
   image: string;
@@ -8,20 +12,19 @@ interface CarouselItem {
 }
 
 const Carousel: FC<{ views: CarouselItem[] }> = (props) => {
-  const panels = props?.views?.map(({ name }) => (
+  const panels = props?.views?.map(({ description, image }) => (
     <Tab.Panel>
-      <img src="/trans.png" alt="" className="h-96" />
+      <img src={image} alt={description} className="h-96" />
     </Tab.Panel>
   ));
   const tabs = props?.views?.map(({ name }) => (
     <Tab as={Fragment}>
       {({ selected }) => (
         <button
-          className={
-            selected
-              ? "border-violet-700 dark:border-violet-300 border-b-2"
-              : ""
-          }
+          className={classNames(
+            "w-full rounded-lg py-2.5 text-sm font-medium leading-5",
+            selected ? "bg-neutral-700 shadow" : "hover:bg-white/[0.12]"
+          )}
         >
           {name}
         </button>
@@ -32,7 +35,9 @@ const Carousel: FC<{ views: CarouselItem[] }> = (props) => {
   return (
     <Tab.Group>
       <Tab.Panels>{panels}</Tab.Panels>
-      <Tab.List className="flex flex-row gap-6 justify-center">{tabs}</Tab.List>
+      <Tab.List className="flex flex-row gap-6 justify-center bg-neutral-800 rounded-lg">
+        {tabs}
+      </Tab.List>
     </Tab.Group>
   );
 };
