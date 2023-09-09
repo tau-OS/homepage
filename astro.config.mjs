dd147382180b76fa5febe6a07aed6d60d478d134
Lleyton Gray <lleyton@fyralabs.com>
2023-09-08 22:40:56 -0700

    chore: add optimizations from our other repo's build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), sitemap(), compress(), robotsTxt()],
 });
